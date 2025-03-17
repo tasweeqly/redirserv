48ab8a2874e0aa4ae7e57abdb9abeae2d1931685
tasweeqly <ali.tiktokcenter1506@gmail.com>
2025-03-18 00:09:42 +0300

    Update redir.js

--- routes/redir.js
+++ routes/redir.js
@@ -6,7 +6,7 @@ const { Users, WhatsNums } = require('../models')
 router.get('/:username', async (req, res) => {
     const username = await Users.findOne({ where: { username: req.params.username } })
     const clientIp = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : req.ip;
-    res.send(`Your real IP address is: ${clientIp}`);
+    res.send(`Your IP address is: ${req.ip} , Your real IP address is: ${clientIp}`);
 
 })
 
