02870df53b7114fc7ef27a5d2a1acf7ed1b74b37
tasweeqly <ali.tiktokcenter1506@gmail.com>
2025-03-18 10:24:32 +0300

    h

--- routes/redir.js
+++ routes/redir.js
@@ -1,13 +1,46 @@
 const express = require('express')
 const router = express.Router()
-const { Users, WhatsNums } = require('../models')
+const { Users, WhatsNums, Ips } = require('../models')
 
 
 router.get('/:username', async (req, res) => {
+    //search for the username in the database
     const username = await Users.findOne({ where: { username: req.params.username } })
+    //bypass the nginxs reverse proxy
     const clientIp = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0].trim() : req.ip;
-    res.send(`Your IP address is: ${req.ip} , Your real IP address is: ${clientIp}`);
+    //res.send(`Your IP address is: ${req.ip} , Your real IP address is: ${clientIp}`);
 
+    //search the database if the client ip was already assigned to a whatsapp number
+    ipdb = await Ips.findOne({where: {UserId: username.id, Ip: clientIp}});
+
+    whatsNum = null
+    //if the client was already assigned to a number
+    if (ipdb) {
+        whatsNum = await WhatsNums.findOne({where: {id: ipdb.WhatsNumId}})
+        whatsNum = whatsNum.WhatsNum
+        console.log(`ip found :${whatsNum}`)
+    }
+    else{
+        //search the database for all the available whatsapp numbers
+        availableWhatsNums = await WhatsNums.findAll({attributes: ['id'], raw: true})
+        //change the returned object to an array of ids
+        const idList = availableWhatsNums.map(item => item.id);
+        //assign new whatsapp number randomly
+        const assignedWhatsNum = await WhatsNums.findOne({where: {id: idList[Math.floor(Math.random() * idList.length)]}})
+        console.log(assignedWhatsNum.WhatsNum)
+
+        //write the new assigned whatsapp number and ip to the database
+        ipnew = await Ips.create({
+            Ip: clientIp,
+            WhatsNumId: assignedWhatsNum.id,
+            UserId: username.id
+        })
+        whatsNum = await WhatsNums.findOne({where: {id: ipnew.WhatsNumId}})
+        whatsNum = whatsNum.WhatsNum
+        console.log(`ip was not found, created new ip and assigned: ${whatsNum}`)
+    }
+    //Ips.destroy({where: {UserId: username.id}})
+    return res.redirect(whatsNum)
 })
 
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { Users, WhatsNums } = require('../models')
 
 
+//create new user by username
 router.post('/create/:username', async (req, res) => {
     
     const username = req.params.username
@@ -21,4 +22,9 @@ router.post('/create/:username', async (req, res) => {
     res.json('hi')
 })
 
+//delete a user by username
+router.delete('/delete/:username', async (req, res) => {
+    Users.destroy({where: {username: req.params.username}})
+    res.json('removed user')
+})
 module.exports = router
